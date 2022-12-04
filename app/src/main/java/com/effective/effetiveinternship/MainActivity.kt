deb9eef5682914fc7e30a8728faa23af55a89d2f
OlegSadJktu <plyuha2004@mail.ru>
2022-12-04 13:32:28 +0600

    Initial.

---
+++ app/src/main/java/com/effective/effetiveinternship/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.effective.effetiveinternship
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.effective.effetiveinternship.ui.theme.EffetiveInternshipTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EffetiveInternshipTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Column(Modifier.padding(all = 12.dp)) {
+        Text(text = "HHello Hello Hello ello $name!", modifier = Modifier.padding(19.dp))
+        Text(text = "HHello Hello Hello ello $name!", modifier = Modifier.padding(19.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    EffetiveInternshipTheme {
+        Greeting("Android")
+    }
+}
